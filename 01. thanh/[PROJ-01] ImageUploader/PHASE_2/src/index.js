115a77a57113d8b1b306a37c61fd356f51761b18
Le Nguyen Duc Thanh <thanhlnd@devcamp.edu.vn>
2022-11-15 17:40:02 +0700

    update15v2

--- 01. thanh/[PROJ-01] ImageUploader/PHASE_2/src/index.js
+++ 01. thanh/[PROJ-01] ImageUploader/PHASE_2/src/index.js
@@ -1,7 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, doc, getDoc } from 'firebase/firestore/lite';
 
-const firebaseApp = initializeApp({ /* config */ });
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyCNBJwdhcUJOQ59Rxz0EEbr71ouoy3SjBs",
+  authDomain: "upimage-101a4.firebaseapp.com",
+  projectId: "upimage-101a4",
+  storageBucket: "upimage-101a4.appspot.com",
+  messagingSenderId: "575808870744",
+  appId: "1:575808870744:web:e14fdb18f82411a6246d2d",
+  measurementId: "G-5R8007LZ8V"
+});
 const db = getFirestore(firebaseApp);
 
 async function loadCity(name) {
@@ -11,4 +19,26 @@ async function loadCity(name) {
     id: snapshot.id,
     ...snapshot.data(),
   };
+}
+
+
+
+
+
+
+function onBtnUploadImg() {
+  "use strict";
+  const ref = firebase.storage().ref();
+  const file = document.querySelector('.inp-upload');
+  const metadata = {
+    contentType: file.name
+  };
+  const name = file.name;
+  const uploadIMG = ref.child(name).put(file, metadata);
+  uploadIMG
+    .then(snapshot => snapshot.ref.getDownloadURL())
+    .then(url => {
+      console.log(url)
+    })
+    .catch(console.error)
 }
