7f203650c18f79c35506190924863ee537b46e3a
NhatLong <lylong323@gmail.com>
2022-12-08 23:13:19 +0700

    [LONG] update

---
+++ 02. long/PROJ-02 React Project/music-player/src/components/Playlist/Audio.js
@@ -0,0 +1,35 @@
+import React,{ useImperativeHandle } from "react"
+import { useRef } from "react"
+
+const Audio = ( props,ref) => {
+    const playerRef = useRef();
+    useImperativeHandle(ref, () => ({
+        play() {
+            playerRef.current.play();
+        },
+        pause() {
+            playerRef.current.pause();
+        },
+        load() {
+            playerRef.current.load();
+        },
+        duration() {
+            return playerRef.current.duration;
+        },
+        currentTime() {
+            return playerRef.current.currentTime;
+        },
+        setCurrentTime(time) {
+            playerRef.current.currentTime = time;
+        }
+
+    }))
+
+    return  (
+        <audio ref={playerRef}>
+                <source src={ props.currentSong } type="audio/ogg"/>
+        </audio>
+    )
+}
+
+export default React.forwardRef(Audio)
--- 02. long/PROJ-02 React Project/music-player/src/components/Playlist/Playlist.js
+++ 02. long/PROJ-02 React Project/music-player/src/components/Playlist/Playlist.js
@@ -1,211 +1,173 @@
 import React from "react";
+import { useState } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
 import '../../assets/css/Main.css';
+import Audio from "./Audio";
 import {PlaylistStyled} from './Playlist-styled'
 
-
-class Playlist extends React.Component {
-    state = {
-        index: 3,
-        currentTime: '0:00',
-        musicList: [
+const musicList = [
             {
                 name: 'Blank Space',
                 artist: 'Taylor Swift',
-                img: 'https://upload.wikimedia.org/wikipedia/vi/7/7c/Taylor_Swift_-_Blank_Space_%28Official_Single_Cover%29.png',
-                audio: 'https://www.bensound.com/bensound-music/bensound-buddy.mp3',
-                duration: '2:02'
+                audio: 'music/songs/s1.mp3',
+                img: 'music/thumbnails/s1.jpg',
+                duration: '5:02'
             },
             {
                 name: 'Girl',
                 artist: 'SYML',
-                img: 'https://www.songmeaningsandfacts.com/wp-content/uploads/2019/06/Roses-by-Chainsmokers-ft-Rozes.png',
-                audio: 'https://www.bensound.com//bensound-music/bensound-sunny.mp3',
+                audio: 'music/songs/s2.mp3',
+                img: 'music/thumbnails/s2.jpg',
                 duration: '2:02'
             },
             {
                 name: 'Habits (Stay Height)',
                 artist: 'Tove Lo',
-                img: 'https://www.songmeaningsandfacts.com/wp-content/uploads/2019/06/Roses-by-Chainsmokers-ft-Rozes.png',
-                audio: 'https://www.bensound.com//bensound-music/the-day.mp3',
+                audio: 'music/songs/s3.mp3',
+                img: 'music/thumbnails/s3.jpg',
                 duration: '2:02'
             },
             {
                 name: 'Love Come Around',
                 artist: 'Elina',
-                img: 'https://www.songmeaningsandfacts.com/wp-content/uploads/2019/06/Roses-by-Chainsmokers-ft-Rozes.png',
-                audio: 'https://www.bensound.com//bensound-music/bensound-sunny.mp3',
+                audio: 'music/songs/s4.mp3',
+                img: 'music/thumbnails/s4.jpg',
                 duration: '2:02'
             },
             {
                 name: 'Roses',
                 artist: 'The Chainsmokers',
-                img: 'https://www.songmeaningsandfacts.com/wp-content/uploads/2019/06/Roses-by-Chainsmokers-ft-Rozes.png',
-                audio: 'https://www.bensound.com//bensound-music/bensound-sunny.mp3',
+                audio: 'music/songs/s5.mp3',
+                img: 'music/thumbnails/s5.jpg',
                 duration: '2:02'
             },
             {
                 name: 'Sunflower',
                 artist: 'Post Malone ft Swae Lee',
-                img: 'https://www.songmeaningsandfacts.com/wp-content/uploads/2019/06/Roses-by-Chainsmokers-ft-Rozes.png',
-                audio: 'https://www.bensound.com//bensound-music/bensound-sunny.mp3',
+                audio: 'music/songs/s6.mp3',
+                img: 'music/thumbnails/s6.jpg',
                 duration: '2:02'
             },
             {
                 name: 'The Scientist',
                 artist: 'Coldplay',
-                img: 'https://www.songmeaningsandfacts.com/wp-content/uploads/2019/06/Roses-by-Chainsmokers-ft-Rozes.png',
-                audio: 'https://www.bensound.com//bensound-music/bensound-sunny.mp3',
+                audio: 'music/songs/s7.mp3',
+                img: 'music/thumbnails/s7.jpg',
                 duration: '2:02'
             },
-        ],
-        pause: false,
-    };
-  
-  
-    componentDidMount() {
-        this.playerRef.addEventListener("timeupdate", this.timeUpdate, false);
-        this.playerRef.addEventListener("ended", this.nextSong, false);
-        this.timelineRef.addEventListener("click", this.changeCurrentTime, false);
-        this.timelineRef.addEventListener("mousemove", this.hoverTimeLine, false);
-        this.timelineRef.addEventListener("mouseout", this.resetTimeLine, false);
-    }
-  
-    componentWillUnmount() {
-        this.playerRef.removeEventListener("timeupdate", this.timeUpdate);
-        this.playerRef.removeEventListener("ended", this.nextSong);
-        this.timelineRef.removeEventListener("click", this.changeCurrentTime);
-        this.timelineRef.removeEventListener("mousemove", this.hoverTimeLine);
-        this.timelineRef.removeEventListener("mouseout", this.resetTimeLine);
-    }
-  
-    changeCurrentTime = (e) => {
-        const duration = this.playerRef.duration;
-        
-        const playheadWidth = this.timelineRef.offsetWidth;
-        const offsetWidht = this.timelineRef.offsetLeft;
+        ];
+
+const Playlist = () => {
+    const [index, setIndex] = useState(0);
+    const [currentTime, setCurrentTime] = useState('0:00');
+    const [pause, setPause] = useState({p: true});
+    const playerRef = useRef();
+    const timelineRef = useRef();
+    const playheadRef = useRef();
+    const hoverPlayheadRef = useRef();
+
+    useEffect(() => {
+        console.log(pause.p)
+        if (!pause.p) {
+            playerRef.current.play();
+        } else {
+            playerRef.current.pause();
+        }
+    }, [pause])
+    
+    useEffect(() => {
+        setPause({p:false})
+    },[index])
+
+    useEffect(() => {
+        setPause({p:true})
+    },[])
+    
+    const changeCurrentTime = (e) => {
+        const duration = playerRef.current.duration();
+        const playheadWidth = timelineRef.current.offsetWidth;
+        const offsetWidht = timelineRef.current.offsetLeft;
         const userClickWidht = e.clientX - offsetWidht;
-    
         const userClickWidhtInPercent = (userClickWidht*100)/playheadWidth;
-    
-        this.playheadRef.style.width = userClickWidhtInPercent + "%";
-        this.playerRef.currentTime = (duration * userClickWidhtInPercent)/100;
+        playheadRef.current.style.width = userClickWidhtInPercent + "%";
+        playerRef.current.setCurrentTime((duration * userClickWidhtInPercent)/100);
     }
-  
-    hoverTimeLine = (e) => {
-        const duration = this.playerRef.duration;
+
+    const hoverTimeLine = (e) => {
+        const duration = playerRef.current.duration();
         
-        const playheadWidth = this.timelineRef.offsetWidth
+        const playheadWidth = timelineRef.current.offsetWidth
         
-        const offsetWidht = this.timelineRef.offsetLeft;
+        const offsetWidht = timelineRef.current.offsetLeft;
         const userClickWidht = e.clientX - offsetWidht;
         const userClickWidhtInPercent = (userClickWidht*100)/playheadWidth;
     
         if(userClickWidhtInPercent <= 100){
-        this.hoverPlayheadRef.style.width = userClickWidhtInPercent + "%";
+        hoverPlayheadRef.current.style.width = userClickWidhtInPercent + "%";
         }
         
         const time = (duration * userClickWidhtInPercent)/100;
         
         if( (time >=0) && (time <= duration)){
-        this.hoverPlayheadRef.dataset.content = this.formatTime(time);
+        hoverPlayheadRef.current.dataset.content = formatTime(time);
         }
     }
-    
-    resetTimeLine = () => {
-        this.hoverPlayheadRef.style.width = 0;
+
+
+    const resetTimeLine = () => {
+        hoverPlayheadRef.current.style.width = 0;
+    }
+
+    const timeUpdate = () => {
+        const duration = playerRef.current.duration();
+        const timelineWidth = timelineRef.current.offsetWidth - playheadRef.current.offsetWidth;
+        const playPercent = 100 * (playerRef.current.currentTime() / duration);
+        playheadRef.current.style.width = playPercent + "%";
+        const currentTime = formatTime(parseInt(playerRef.current.currentTime()));
+        setCurrentTime(
+            currentTime
+        );
     }
     
-    timeUpdate = () => {
-        const duration = this.playerRef.duration;
-        const timelineWidth = this.timelineRef.offsetWidth - this.playheadRef.offsetWidth;
-        const playPercent = 100 * (this.playerRef.currentTime / duration);
-        this.playheadRef.style.width = playPercent + "%";
-        const currentTime = this.formatTime(parseInt(this.playerRef.currentTime));  
-        this.setState({ 
-        currentTime 
-        });
-    }
-  
-    formatTime = (currentTime) =>{
+    const formatTime = (currentTime) => {
         const minutes = Math.floor(currentTime / 60);
         let seconds = Math.floor(currentTime % 60);
-    
         seconds = (seconds >= 10) ? seconds : "0" + seconds % 60;
-        
         const formatTime = minutes + ":" +  seconds
-    
         return formatTime;
     }
-  
-    updatePlayer = () =>{
-        const { musicList, index } = this.state;
-        const currentSong = musicList[index];
-        const audio = new Audio(currentSong.audio);
-        this.playerRef.load();
+
+    const updatePlayer = () =>{
+        playerRef.current.load();
     }
-    
-    nextSong = () => {
-        const { musicList, index, pause } = this.state;
-        
-        this.setState({ 
-            index: (index + 1) % musicList.length
-        });
-        this.updatePlayer();
-        if(pause){
-            this.playerRef.play();
-        }
+
+    const nextSong = () => { 
+        setIndex((index + 1) % musicList.length)
+        updatePlayer();
     };
-  
-    prevSong = () => {
-        const { musicList, index, pause } = this.state;  
-        
-        this.setState({ 
-            index: (index + musicList.length - 1) % musicList.length
-        });
-        this.updatePlayer();
-        if(pause){
-            this.playerRef.play();
-        }
+     const prevSong = () => {
+        updatePlayer();
+        setIndex((index + musicList.length - 1) % musicList.length);
     };
-     
-  
-    playOrPause = () =>{
-        const { musicList, index, pause } = this.state;
-        const currentSong = musicList[index];
-        const audio = new Audio(currentSong.audio);
-        if( !this.state.pause ){
-            this.playerRef.play();
-        }else{
-            this.playerRef.pause();
-        }
-        this.setState({
-            pause: !pause
-        })
+    const playOrPause = () => {
+        const cur = !pause.p;
+        console.log(cur)
+        setPause({p:!pause.p})
     }
-    
-    clickAudio = (key) =>{
-        const { pause } = this.state;
-        
-        this.setState({
-            index: key
-        });
-        
-        this.updatePlayer();
-        if(pause){
-            this.playerRef.play();
-        }
+    const  clickAudio = (key) =>{
+     updatePlayer();
+     setIndex(key)
     }
-  
-    
-    render() {
-        const { musicList, index, currentTime, pause } = this.state;
-        const currentSong = musicList[index];
-        return (
+    const currentSong = musicList[index];
+
+    return (
             <PlaylistStyled>
-                <div className="current-song">
-                    <audio ref={ref => this.playerRef = ref}>
-                        <source src={ currentSong.audio } type="audio/ogg"/>
-                    </audio>
+                <div className="current-song" onTimeUpdate={timeUpdate} onEnded={nextSong} >
+                    <Audio ref={playerRef} currentSong={currentSong.audio} ></Audio>
+                    {/* <audio ref={ref => playerRef = ref}>
+                         <source src={ currentSong.audio } type="audio/ogg"/>
+                     </audio> */}
                     <div className="song-info">
                         <div className="img-wrap">
                             <img src={ currentSong.img }/>
@@ -218,38 +180,35 @@ class Playlist extends React.Component {
 
                     <div className="time-controls">
                         <div className="controls">
-                            <button onClick={this.prevSong} className="prev prev-next current-btn"><i className="fas fa-backward"></i></button>
+                            <button onClick={prevSong} className="prev prev-next current-btn"><i className="fas fa-backward"></i></button>
                             
-                            <button onClick={this.playOrPause} className="play current-btn">
+                            <button onClick={playOrPause} className="play current-btn">
                                 {
-                                (!pause) ? <i className="fas fa-play"></i>
+                                (pause.p) ? <i className="fas fa-play"></i>
                                 :<i class="fas fa-pause"></i>
                                 }
                             </button>
-                            <button onClick={this.nextSong} className="next prev-next current-btn"><i className="fas fa-forward"></i></button>
+                            <button onClick={nextSong} className="next prev-next current-btn"><i className="fas fa-forward"></i></button>
                         </div>
                         
                         <div className="time">
                             <div className="current-time">{ currentTime }</div>
-                            <div ref={ref => this.timelineRef = ref} id="timeline">
-                                <div ref={ref => this.playheadRef = ref} id="playhead"></div>
-                                <div ref={ref => this.hoverPlayheadRef = ref} class="hover-playhead" data-content="0:00"></div>
+                            <div ref={timelineRef} id="timeline" onClick={changeCurrentTime} onMouseMove={hoverTimeLine} onMouseOut={resetTimeLine}>
+                                <div ref={playheadRef} id="playhead"></div>
+                                <div ref={hoverPlayheadRef} className="hover-playhead" data-content="0:00"></div>
                             </div>
                             <div className="end-time">{ currentSong.duration }</div>
                         </div>
                     </div>
-                    
-        
-                    
                 </div>
 
                 <div className="play-list" >
                     {musicList.map( (music, key=0) =>
-                        <div key={key} 
-                            onClick={()=>this.clickAudio(key)}
-                            className={"track " + 
-                                (index === key && !pause ?'current-audio':'') + 
-                                (index === key && pause ?'play-now':'')} 
+                        <div key={key}
+                            onClick={()=>clickAudio(key)}
+                            className={"track " +
+                                (index === key && !pause ?'current-audio':'') +
+                                (index === key && pause ?'play-now':'')}
                         >
                             
                             <img className="track-img" src={music.img}/>
@@ -257,10 +216,7 @@ class Playlist extends React.Component {
                                 <div className="track-name" >{music.name}</div>
                                 <div className="track-author" >{music.artist}</div>
                                 <div className="track-duration" >
-                                    {(index === key)
-                                        ?currentTime
-                                        :music.duration
-                                    }
+                                    {music.duration}
                                 </div>
                             </div>
                             
@@ -269,7 +225,5 @@ class Playlist extends React.Component {
                 </div>
             </PlaylistStyled>
         )
-    }
 }
-  
-export default Playlist
+export default Playlist;
